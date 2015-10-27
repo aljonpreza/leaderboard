bd3302aad2bee2b37c5765427c9891bf1e3286f8
Aljon Preza <aljonpreza@yahoo.com>
2015-10-27 10:11:23 -1000

    Finished "Sessions"

--- leaderboard.js
+++ leaderboard.js
@@ -12,3 +12,51 @@ PlayersList = new Mongo.Collection('players'); // No var makes it global
 
 /** Counts the number of documents
  * PlayersList.find().count(); **/
+
+// Condition to get statement from command line
+if (Meteor.isServer) {
+
+}
+
+// Condition to get statement from console
+if (Meteor.isClient) {
+  /** Template <= class
+   * Looks for name of template
+   * Name given to the function
+   * Template.leaderboard.player = function() {
+    return "Some other text"
+  } **/
+
+  // Helps to create multiple helper functions
+  Template.leaderboard.helpers({
+    // helper functions go here
+
+    /** Player function
+     * name of helper = key with the associated function as the value
+     */
+    'player': function() {
+      return PlayersList.find();
+    }, // <= Make sure to use commas for multiple helper functions!
+
+    'otherHelperFunction': function() {
+      return "Some other function"
+    }
+  });
+
+  // Events allow us to trigger execution code when a user on a button, etc.
+  Template.leaderboard.events({
+    // events go here
+
+    /**
+     * Second argument determines the specific element to work on
+     */
+    'click .player': function(){
+      /** Stores small data that is not saved in database, won't be remembered
+       * first = name for the session
+       * second = passing through
+       */
+      Session.set('selectedPlayer', 'session value test');
+      console.log("You clicked an .player element");
+    }
+  });
+}
