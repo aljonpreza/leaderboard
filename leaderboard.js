54d4b107408b11dc9a344195b9fe9bcb3ff6446c
Aljon Preza <aljonpreza@yahoo.com>
2015-10-28 11:25:59 -1000

    Now on forms

--- leaderboard.js
+++ leaderboard.js
@@ -35,11 +35,28 @@ if (Meteor.isClient) {
      * name of helper = key with the associated function as the value
      */
     'player': function() {
-      return PlayersList.find();
+      // {} gets data of the table
+      // {sort: {score: -1}} sort te data in descending order 1 for ascending
+      return PlayersList.find({}, {sort: {score: -1, name: 1}});
     }, // <= Make sure to use commas for multiple helper functions!
 
     'otherHelperFunction': function() {
-      return "Some other function"
+      return "Some other function";
+    },
+
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      // return this._id; // Puts ID in HTML, since in each block, has access to data
+
+      if (playerId == selectedPlayer) {
+        return "selected"; // returns the selected class
+      }
+    },
+
+    'showSelectedPlayer': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer); //pass through unique Id
     }
   });
 
@@ -50,13 +67,34 @@ if (Meteor.isClient) {
     /**
      * Second argument determines the specific element to work on
      */
-    'click .player': function(){
+    'click .player': function() {
+      /**
+       * this = document of player that has been clicked
+       * _id = name of the field that contains the id
+       */
+      var playerID = this._id;
+
       /** Stores small data that is not saved in database, won't be remembered
        * first = name for the session
        * second = passing through
        */
-      Session.set('selectedPlayer', 'session value test');
-      console.log("You clicked an .player element");
+      Session.set('selectedPlayer', playerID);
+
+      /** Prints the ID of the player
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer); */
+    },
+
+    'click .increment': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      //PlayersList.update(selectedPlayer, {$set: {score: 5}}); Sets score by 5 using ID
+      PlayersList.update(selectedPlayer, {$inc: {score: 5}}); // increments score by 5
+    },
+
+    'click .decrement': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      //PlayersList.update(selectedPlayer, {$set: {score: 5}}); Sets score by 5 using ID
+      PlayersList.update(selectedPlayer, {$inc: {score: -5}}); // increments score by 5
     }
   });
 }
